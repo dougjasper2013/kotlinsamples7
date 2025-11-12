4c1c89cdc8f333f5edcd962414ef0f1270905d9a
Douglas Jasper <doug.jasper@trios.com>
2025-11-12 09:17:49 -0500

    Added Functions

--- src/main/java/org/example/functions.kt
+++ src/main/java/org/example/functions.kt
@@ -1,2 +1,12 @@
 package org.example
 
+fun main() {
+    var intro = printName("Doug Jasper") // calling or invoking the function
+    println(intro)
+}
+
+fun printName(name: String): String { // function definition
+    println("My name is $name")
+    var greeting = "Hi $name, how are you?"
+    return greeting
+}
